de7194334f521ee2d04a81d9a82d859a36016a38
BWalzer <bwalzer@me.com>
2017-05-30 22:28:29 -0700

    Add files via upload
    Updated 5-30 by Ben Walzer. I fully implemented the new hyperbola_points() function, rewrote the error analysis part and added a lot more comments to help readability.

--- Crow_2D_Simulation.m
+++ Crow_2D_Simulation.m
@@ -43,10 +43,10 @@ Fmax = 3500;                                                               % Max
 % Create the crow signal. If the function was called without values for
 % x_s, y_s or z_s, default values will be assigned.
 if ~exist('x_s','var')
-    x_s = 0.6;
+    x_s = 0.7;
 end
 if ~exist('y_s','var')
-    y_s = 0.7; 
+    y_s = 0.2; 
 end
 if ~exist('z_s','var')
     z_s = 0;
@@ -148,7 +148,6 @@ cor_12 = xcorr(data1,data2,'coef');
 cor_13 = xcorr(data1,data3,'coef');
 cor_14 = xcorr(data1,data4,'coef');
 cor_34 = xcorr(data3,data4,'coef');
-% made some changes here; insert correlation from 2 to 4 and 2 to 3
 cor_24 = xcorr(data2,data4, 'coeff');
 cor_23 = xcorr(data2,data3, 'coeff');
 
@@ -156,7 +155,6 @@ cor_23 = xcorr(data2,data3, 'coeff');
 [~, max_ind_13] = max(cor_13);
 [~, max_ind_14] = max(cor_14);
 [~, max_ind_34] = max(cor_34);
-% added max index 2 to 4 and 2 to 3 here
 [~, max_ind_24] = max(cor_24);
 [~, max_ind_23] = max(cor_23);
 
@@ -189,20 +187,22 @@ else
     t_max_34 = -t(L-max_ind_34+1);
 end   
 
-% time delay between element 2 and 4, added by Ben Walzer
+% time delay between element 2 and 4
 if max_ind_24 >= L    
     t_max_24 = t(max_ind_24-L+1);
 else
     t_max_24 = -t(L-max_ind_24+1);
 end
 
-% time delay between element 2 and 3, added by Ben Walzer
+% time delay between element 2 and 3
 if max_ind_23 >= L    
     t_max_23 = t(max_ind_23-L+1); 
 else
     t_max_23 = -t(L-max_ind_23+1);
 end   
 
+%The following code is not necessary for any of the calculations. It is
+%often left commented because it creates unnecessary plots. 
 % figure(3)
 % subplot(4,1,1)
 % hold all
@@ -222,27 +222,42 @@ end
 % title_name = strcat( 'Time Delay 34: ',num2str(t_max_34),' sec'); title(title_name,'fontsize',18);
 
 %% **************************** HYPERBOLA **********************************
-%number of points on the hyperbola
+%This section creates the hyperbolas that arise from the time difference of
+%arrivals that we calculated above. There are 6 hyperbolas between the
+%4 microphones. 
+
+%The hyperbolas that are between mics 1_2, 1_3, 2_4, 3_4 are simple to
+%create (i.e, the hyperbola_points function will do everything for you with
+%the right values). However, the hyperbolas between 1_4 and 2_3 are more
+%difficult because they require rotation after the fact, and some
+%translations.
+
+%number of points on the hyperbola, more points results in a more precise
+%calculation, but will take more time to calculate.
 point_num = 10000;
 
-%1 and 2, BLACK
-a_12 = abs(t_max_12)*c/2;
-c_12 =  sqrt((x_r(1)-x_r(2))^2+(y_r(1)-y_r(2))^2+(z_r(1)-z_r(2))^2);
-b_12 = sqrt(c_12^2-a_12^2);
-x_mid_12 = (x_r(1)+x_r(2))/2;
-y_mid_12 = (y_r(1)+y_r(2))/2;
-h_12 = create_hyperbola(a_12,b_12,0,x_mid_12,y_mid_12,1,point_num);
+%Hyperbola between mics 1 and 2, plotted in BLACK
+a_12 = abs(t_max_12)*c/2; %a value for the hypoerbola
+c_12 =  sqrt((x_r(1)-x_r(2))^2+(y_r(1)-y_r(2))^2+(z_r(1)-z_r(2))^2); %c value for the hyperbola
+b_12 = sqrt(c_12^2-a_12^2); %b value for the hyperbola
+x_mid_12 = (x_r(1)+x_r(2))/2; %calculates the x-value at the midpoint between the two microphones
+y_mid_12 = (y_r(1)+y_r(2))/2; %calculates the y-value at the midpoint between the two microphones
+%hyperbola_points is a function that takes all the different parts of the
+%hyperbola, and creates a matrix of size 2 x (point_num*2+1), where the
+%first row is the x values and the second row is the y values. See the
+%hyperbola_points function for more detail
+[h_12(1,:), h_12(2,:)] = hyperbola_points(a_12, b_12, x_mid_12, y_mid_12, x_r(1)-5, x_r(4)+5, y_r(1)-5, y_r(4)+5, point_num, 1);
 
-%1 and 3 RED
+%Hyperbola between mics 1 and 3, plotted in RED
 a_13 = abs(t_max_13)*c/2;
 c_13 =  sqrt((x_r(1)-x_r(3))^2+(y_r(1)-y_r(3))^2+(z_r(1)-z_r(3))^2); 
 b_13 = sqrt(c_13^2-a_13^2);
 x_mid_13 = (x_r(1)+x_r(3))/2; 
 y_mid_13 = (y_r(1)+y_r(3))/2;
-h_13 = create_hyperbola(a_13,b_13,0,x_mid_13,y_mid_13,2,point_num); %changed the hyperbola style to 2, rotates it 90 degrees (BW)
+[h_13(1,:), h_13(2,:)] = hyperbola_points(a_13, b_13, x_mid_13, y_mid_13, x_r(1)-5, x_r(4)+5, y_r(1)-5, y_r(4)+5, point_num, 2);
 
 
-%1 and 4 PURPLE
+%Hyperbola between mics 1 and 4, plotted in PURPLE
 %(BW) I'm attempting to plot the hyperbola as if point 4 was on the x axis,
 %preserving distance between the points
 x_4_rotated = x_r(1) + sqrt((x_r(4) - x_r(1))^2 + (y_r(4) - y_r(1))^2); %preserving the distance between points 1 and 4
@@ -252,7 +267,7 @@ c_14 = sqrt((x_r(1) - x_4_rotated)^2 + (y_r(1) - y_4_rotated)^2 + (z_r(1) - z_r(
 b_14 = sqrt(c_14^2-a_14^2);
 x_mid_14 = (x_r(1) + x_4_rotated)/2;
 y_mid_14 = (y_r(1) + y_4_rotated)/2;
-[h_14] = create_hyperbola(a_14,b_14,0,x_mid_14,y_mid_14,1,point_num);
+[h_14(1,:), h_14(2,:)] = hyperbola_points(a_14, b_14, x_mid_14, y_mid_14, x_r(1)-5, x_r(4)+5, y_r(1)-5, y_r(4)+5, point_num, 1);
 
 %Because the h_14 hyperbolas was initially calculated as if recorder 4 was
 %inline horizontally with recorder 1, the hyperbola needs to be rotated up
@@ -263,21 +278,19 @@ theta_14 = atan(abs(y_r(4) - y_r(1)) / abs(x_r(4) - x_r(1)));
 
 %the 4x8 rotation matrix, this rotates the hyperbola by theta_14 degrees
 %counterclockwise, centered at the origin.
-rotation = [cos(theta_14) -sin(theta_14) 0 0;
-            sin(theta_14) cos(theta_14) 0 0 ;
-            0 0 cos(theta_14) -sin(theta_14);
-            0 0 sin(theta_14) cos(theta_14)];
-h_14 = rotation*h_14;
+rotation = [cos(theta_14) -sin(theta_14);
+             sin(theta_14) cos(theta_14)];
+h_14 = rotation * h_14;
 
-%3 and 4 BLUE
+%Hyperbola between mics 3 and 4, plotted in BLUE
 a_34 = abs(t_max_34)*c/2;
 c_34 =  sqrt((x_r(3)-x_r(4))^2+(y_r(3)-y_r(4))^2+(z_r(3)-z_r(4))^2); 
 b_34 = sqrt(c_34^2-a_34^2);
 x_mid_34 = (x_r(3)+x_r(4))/2;
 y_mid_34 = (y_r(3)+y_r(4))/2;
-h_34 = create_hyperbola(a_34,b_34,0,x_mid_34,y_mid_34,1,point_num);
+[h_34(1,:), h_34(2,:)] = hyperbola_points(a_34, b_34, x_mid_34, y_mid_34, x_r(1)-5, x_r(4)+5, y_r(1)-5, y_r(4)+5, point_num, 1);
 
-%2 and 3 Yellow
+%Hyperbola between mics 2 and 3, plotted in Green
 x_3_rotated = x_r(2) - sqrt((x_r(2) - x_r(3))^2 + (y_r(2) - y_r(3))^2);
 y_3_rotated = y_r(2);
 a_23 = abs(t_max_23)*c/2;
@@ -285,181 +298,124 @@ c_23 = sqrt((x_r(3) - x_3_rotated)^2 + (y_r(3) - y_3_rotated)^2); %excluding z f
 b_23 = sqrt(c_23^2 - a_23^2);
 x_mid_23 = (x_3_rotated + x_r(2))/2;
 y_mid_23 = (y_3_rotated + y_r(2))/2;
-h_23 = create_hyperbola(a_23, b_23, 0, x_mid_23, y_mid_23, 1, point_num); 
+[h_23(1,:), h_23(2,:)] = hyperbola_points(a_23, b_23, x_mid_23, y_mid_23, x_r(1)-5, x_r(4)+5, y_r(1)-5, y_r(4)+5, point_num, 1);
 
-%shift the hyperbola left for ritation
+%shift the hyperbola over, so it can be rotated about the origin
 h_23(1,:) = h_23(1,:) - x_r(2);
-h_23(3,:) = h_23(3,:) - x_r(2);
-%rotate the hypberola
-theta_23 = -atan(abs(y_r(3) - y_r(2)) / abs(x_r(3) - x_r(2)));
-rotation = [cos(theta_23) -sin(theta_23) 0 0;
-            sin(theta_23) cos(theta_23) 0 0 ;
-            0 0 cos(theta_23) -sin(theta_23);
-            0 0 sin(theta_23) cos(theta_23)];
-h_23 = rotation*h_23;
-%shift the hyperbola back (undoing the first shift)
-h_23(1,:) = h_23(1,:) + x_r(2);
-h_23(3,:) = h_23(3,:) + x_r(2);
 
-%2 and 4 Cyan
+%rotation matrix, rotates theta_23 radians counterclockwise, centered about
+%the origin
+theta_23 = -atan(abs(y_r(3) - y_r(2)) / abs(x_r(3) - x_r(2)));
+rotation = [cos(theta_23) -sin(theta_23);
+             sin(theta_23) cos(theta_23)];
+h_23 = rotation * h_23;
+h_23(1,:) = h_23(1,:) + x_r(2);
+
+%Hyperbola between mics 2 and 4, plotted in Cyan
 a_24 = abs(t_max_24)*c/2;
 c_24 = sqrt((x_r(2) - x_r(4))^2 + (y_r(2) - y_r(4))^2 + (z_r(2) - z_r(4))^2);
 b_24 = sqrt(c_24^2 - a_24^2);
 x_mid_24 = (x_r(2) + x_r(4))/2;
 y_mid_24 = (y_r(2) + y_r(4))/2;
-h_24 = create_hyperbola(a_24, b_24, 0, x_mid_24, y_mid_24, 2, point_num); %changed the hyperbola style to 2, rotates it 90 degrees (BW)
+[h_24(1,:), h_24(2,:)] = hyperbola_points(a_24, b_24, x_mid_24, y_mid_24, x_r(1)-5, x_r(4)+5, y_r(1)-5, y_r(4)+5, point_num, 2);
+
 %% plotting the hyperbolas
 if hyp_plot == true %hyp_plot == TRUE -> plot hyperbolas, hyp_plot == FALSE -> no plotting
-    figure (7)
-    xlim([x_r(1)-.01 x_r(2)+0.01]);
-    ylim([y_r(1)-0.01 x_r(2)+.01]);
-    hold all
-    plot(x_s,y_s,'dk','MarkerFaceColor','y','markersize',14,'LineWidth',1);
-    plot(x_r(1),y_r(1),'ob','MarkerFaceColor','b','markersize',14,'LineWidth',1); 
+    figure (7); %creates a new window
+    xlim([x_r(1)-.01 x_r(2)+0.01]); %restricts the plot window 
+    ylim([y_r(1)-0.01 x_r(2)+.01]); %restricts the plot window
+
+    hold on; %stops changes being made to the configuration of the plots
+    plot(x_s,y_s,'dk','MarkerFaceColor','y','markersize',14,'LineWidth',1); %plots a Green diamond at the simulated source location
+    plot(x_r(1),y_r(1),'ob','MarkerFaceColor','b','markersize',14,'LineWidth',1); %plots a blue dot at the location of the four microphones
     plot(x_r(2),y_r(2),'ob','MarkerFaceColor','b','markersize',14,'LineWidth',1); 
     plot(x_r(3),y_r(3),'ob','MarkerFaceColor','b','markersize',14,'LineWidth',1);
     plot(x_r(4),y_r(4),'ob','MarkerFaceColor','b','markersize',14,'LineWidth',1);
+    legend('Actual Source Location','Receivers Location') %adds a legend to the plot
 
-    % 1 and 2: Black
+    %Plots the hyperbola between mics 1 and 2: Black
     plot(h_12(1,:),h_12(2,:),'k'); 
-    plot(h_12(3,:),h_12(4,:),'k');
 
-    %1 and 3: Red
+    %Plots the hyperbola between mics 1 and 3: Red
     plot(h_13(1,:),h_13(2,:),'r'); 
-    plot(h_13(3,:),h_13(4,:),'r');
 
-    %1 and 4: Purple
+    %Plots the hyperbola between mics 1 and 4: Purple
     plot(h_14(1,:), h_14(2,:), 'm');
-    plot(h_14(3,:), h_14(4,:), 'm');
 
-    %3 and 4: Blue
+    %Plots the hyperbola between mics 3 and 4: Blue
     plot(h_34(1,:),h_34(2,:),'b'); 
-    plot(h_34(3,:),h_34(4,:),'b');
-
-    %2 and 4: Cyan
+    
+    %Plots the hyperbola between mics 2 and 4: Cyan
     plot(h_24(1,:), h_24(2,:), 'c');
-    plot(h_24(3,:), h_24(4,:), 'c');
 
-    %2 and 3: Yellow
-    plot(h_23(1,:), h_23(2,:), 'y'); 
-    plot(h_23(3,:), h_23(4,:), 'y');
-
-    legend('Actual Source Location','Receivers Location')
+    %Plots the hyperbola between mics 2 and 3: Green   
+    plot(h_23(1,:), h_23(2,:), 'g');
 end
 %% **************************** Testing Accuracy **********************************
-%The purpose of this section is to calculate the distance between the test
-%point and point closest on each hyperbola. 
+% %The purpose of this section is to calculate the distance between the test
+% %point and point closest on each hyperbola. 
 
-%gets the size of the points where the hyperbola points are stored
-%Note that the size of each hyperbola should be the same, so these values will be reused for the other checks
-[rows, cols] = size(h_12);                                                  
+%gets the dimension of the hyperbola h_12. Note that this dimension should be the same for each hyperbola. 
+%Should always be a 2 x (point_num*2+1) matrix
+[rows, cols] = size(h_12); 
 
-%Minimize Hyperbola 1-2
-%min_12 is the point at where the hyperbola is closest to the sample point,
-%and its distance from the sample at that point.
-%It's initial value is the first point in the hyperbola
-min_12 = [h_12(1,1), h_12(2,1), sqrt((h_12(1,1) - x_s)^2 + (h_12(2,1) - y_s)^2)];  
+%sets the initial minimum to be at (0,0) and its distance to be infinity
+min_12 = [0, 0, inf];
 
-%loops through all points of hyperbola between mics 1 and 2
-%euclidean distance between the point on the hyperbola and the sample point
-%checks if a new point is closer than the previous closest
-%if new closest point is found, sets it as the closest point
-for i = 1:cols                                                              
-    distance_12a = sqrt((h_12(1,i) - x_s)^2 + (h_12(2,i) - y_s)^2);
-    distance_12b = sqrt((h_12(3,i) - x_s)^2 + (h_12(4,i) - y_s)^2);
-   if  distance_12a < min_12(3)                                                
-       min_12 = [h_12(1,i), h_12(2,i), distance_12a];
-   end
-   if distance_12b < min_12(3)
-       min_12 = [h_12(3,i), h_12(4,i), distance_12b];
-   end
-end
-if hyp_plot == true
-    plot (min_12(1), min_12(2), 'sk', 'markersize', 10, 'markerfacecolor', 'k');
-end
-
-%Minimize Hyperbola 1-3
-min_13 = [h_13(1,1), h_13(2,1), sqrt((h_13(1,1) - x_s)^2 + (h_13(2,1) - y_s)^2)];
+%goes through every set of points in the hyperbola matrix
 for i = 1:cols
-    distance_13a = sqrt((h_13(1,i) - x_s)^2 + (h_13(2,i) - y_s)^2);
-    distance_13b = sqrt((h_13(3,i) - x_s)^2 + (h_13(4,i) - y_s)^2);
-    if distance_13a < min_13(3)
-        min_13 = [h_13(1,i), h_13(2,i), distance_13a];
+    %calculates the euclidean distance between the current point and the
+    %simulated source location
+    distance = sqrt((h_12(1,i) - x_s)^2 + (h_12(2,i) - y_s)^2);
+    
+    %if the current distance is less than the smallest so far, the current
+    %point is set as the new minimum
+    if distance < min_12(3)
+        min_12 = [h_12(1,i), h_12(2,i), distance];
     end
-    if distance_13b < min_13(3)
-        min_13 = [h_13(3,i), h_13(4,i), distance_13b];        
-    end
-end
-if hyp_plot == true
-    plot (min_13(1), min_13(2), 'sr', 'markersize', 10, 'markerfacecolor', 'r');
 end
 
-%Minimize Hyperbola 1-4
-min_14 = [h_14(1,1), h_14(2,1), sqrt((h_14(1,1) - x_s)^2 + (h_14(2,1) - y_s)^2)];
-for i = 1:cols                                                              
-    distance_14a = sqrt((h_14(1,i) - x_s)^2 + (h_14(2,i) - y_s)^2);
-    distance_14b = sqrt((h_14(3,i) - x_s)^2 + (h_14(4,i) - y_s)^2);
-   if  distance_14a < min_14(3)                                                
-       min_14 = [h_14(1,i), h_14(2,i), distance_14a];
-   end
-   if distance_14b < min_14(3)
-       min_14 = [h_14(3,i), h_14(4,i), distance_14b];
-   end
-end
-if hyp_plot == true
-    plot (min_14(1), min_14(2), 'sm', 'markersize', 10, 'markerfacecolor', 'm');
-end
-
-% %Minimize Hyperbola 2-3
-min_23 = [h_23(1,1), h_23(2,1), sqrt((h_23(1,1) - x_s)^2 + (h_23(2,1) - y_s)^2)];
-for i = 1:cols                                                              
-    distance_23a = sqrt((h_23(1,i) - x_s)^2 + (h_23(2,i) - y_s)^2);
-    distance_23b = sqrt((h_23(3,i) - x_s)^2 + (h_23(4,i) - y_s)^2);
-   if  distance_23a < min_23(3)                                                
-       min_23 = [h_23(1,i), h_23(2,i), distance_23a];
-   end
-   if distance_23b < min_23(3)
-       min_23 = [h_23(3,i), h_23(4,i), distance_23b];
-   end
-end
-if hyp_plot == true
-    plot (min_23(1), min_23(2), 'sy', 'markersize', 10, 'markerfacecolor', 'y');
-end
-%Minimize Hyperbola 2-4
-min_24 = [h_24(1,1), h_24(2,1), sqrt((h_24(1,1) - x_s)^2 + (h_24(2,1) - y_s)^2)];
+min_13 = [0, 0, inf];
 for i = 1:cols
-    distance_24a = sqrt((h_24(1,i) - x_s)^2 + (h_24(2,i) - y_s)^2);
-    distance_24b = sqrt((h_24(3,i) - x_s)^2 + (h_24(4,i) - y_s)^2);
-     current = [h_24(1,i), h_24(2,i), distance_24a;
-                h_24(3,i), h_24(4,i), distance_24b];
-    if distance_24a < min_24(3)
-        min_24 = [h_24(1,i), h_24(2,i), distance_24a];
+    distance = sqrt((h_13(1,i) - x_s)^2 + (h_13(2,i) - y_s)^2);
+    if distance < min_13(3)
+        min_13 = [h_13(1,i), h_13(2,i), distance];
     end
-    if distance_24b < min_24(3)
-        min_24 = [h_24(3,i), h_24(4,i), distance_24b];        
-    end
-end
-if hyp_plot == true
-    plot (min_24(1), min_24(2), 'sc', 'markersize', 10, 'markerfacecolor', 'c');
-end
-% %Minimize Hyperbola 3-4
-min_34 = [h_34(1,1), h_34(2,1), sqrt((h_34(1,1) - x_s)^2 + (h_34(2,1) - y_s)^2)];
-for i = 1:cols
-    distance_34a = sqrt((h_34(1,i) - x_s)^2 + (h_34(2,i) - y_s)^2);
-    distance_34b = sqrt((h_34(3,i) - x_s)^2 + (h_34(4,i) - y_s)^2);
-     current = [h_34(1,i), h_34(2,i), distance_34a;
-                h_34(3,i), h_34(4,i), distance_34b];
-    if distance_34a < min_34(3)
-        min_34 = [h_34(1,i), h_34(2,i), distance_34a];
-    end
-    if distance_34b < min_34(3)
-        min_34 = [h_34(3,i), h_34(4,i), distance_34b];        
-    end
-end
-if hyp_plot == true
-    plot (min_34(1), min_34(2), 'sb', 'markersize', 10, 'markerfacecolor', 'b');
 end
 
+min_14 = [0, 0, inf];
+for i = 1:cols
+    distance = sqrt((h_14(1,i) - x_s)^2 + (h_14(2,i) - y_s)^2);
+    if distance < min_14(3)
+        min_14 = [h_14(1,i), h_14(2,i), distance];
+    end
+end
+
+min_23 = [0, 0, inf];
+for i = 1:cols
+    distance = sqrt((h_23(1,i) - x_s)^2 + (h_23(2,i) - y_s)^2);
+    if distance < min_23(3)
+        min_23 = [h_23(1,i), h_23(2,i), distance];
+    end
+end
+
+min_24 = [0, 0, inf];
+for i = 1:cols
+    distance = sqrt((h_24(1,i) - x_s)^2 + (h_24(2,i) - y_s)^2);
+    if distance < min_24(3)
+        min_24 = [h_24(1,i), h_24(2,i), distance];
+    end
+end
+
+min_34 = [0, 0, inf];
+for i = 1:cols
+    distance = sqrt((h_34(1,i) - x_s)^2 + (h_34(2,i) - y_s)^2);
+    if distance < min_34(3)
+        min_34 = [h_34(1,i), h_34(2,i), distance];
+    end
+end
+
+%sets the return values (see first line of this function)
 min_dist_12 = min_12(3);
 min_dist_23 = min_23(3);
 min_dist_34 = min_34(3);
@@ -467,27 +423,12 @@ min_dist_14 = min_14(3);
 min_dist_13 = min_13(3);
 min_dist_24 = min_24(3);
 
-%% ************************* SEARCH FOR LOCATION ***************************
-% for x_ind = 1:length(x_grid)
-%     Error (x_ind) = abs(y_aft(x_ind)-y_starboard_upper(x_ind));
-% end
-% [~, ind_min] = min(Error);
-        
-
-% figure (5)
-% hold all
-% plot(port_upper(1),port_upper(3),'or','MarkerFaceColor','r');
-% % plot(port_lower(1),port_lower(3),'or');
-% plot(starboard_upper(1),starboard_upper(3),'ob','MarkerFaceColor','b');
-% % plot(starboard_lower(1),starboard_lower(3),'or');
-% plot(aft(1),aft(3),'ok','MarkerFaceColor','k');
-% legend('Port Upper','Starboard Upper','AFT')
-% plot(x_grid,y_starboard_upper1,'--b');plot(x_grid,y_aft1,'--k');
-% plot(x_grid,y_starboard_upper2,'--b');plot(x_grid,y_aft2,'--k');
-% plot(y_starboard_upper3(1,:),y_starboard_upper3(2,:),'-b')
-% plot(y_aft3(1,:),y_aft4(2,:),'-k')
-% plot(y_starboard_upper3(3,:),y_starboard_upper3(4,:),'-b')
-% plot(y_aft3(3,:),y_aft3(4,:),'-k')
-% 
-% xlabel('x(m)')
-% ylabel('z(m)')
+%plots the points closest to source location
+if hyp_plot == true
+    plot (min_12(1), min_12(2), 'sk', 'markersize', 10, 'markerfacecolor', 'k');
+    plot (min_13(1), min_13(2), 'sk', 'markersize', 10, 'markerfacecolor', 'r');
+    plot (min_14(1), min_14(2), 'sk', 'markersize', 10, 'markerfacecolor', 'm');
+    plot (min_23(1), min_23(2), 'sk', 'markersize', 10, 'markerfacecolor', 'g');
+    plot (min_24(1), min_24(2), 'sk', 'markersize', 10, 'markerfacecolor', 'c');
+    plot (min_34(1), min_34(2), 'sk', 'markersize', 10, 'markerfacecolor', 'b');
+end
